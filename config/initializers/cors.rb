94dd0a2156c2c8dc0b20952f898c4c03555fa9ca
alagaesia93 <ale.macagno@hotmail.it>
2019-03-27 15:00:36 +0100

    frontend cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://topclean-backend.herokuapp.com']
+    origins ['http://localhost:3000', 'https://topclean-frontend.herokuapp.com']
 
     resource  '*',
               headers: :any,
