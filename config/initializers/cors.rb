46d0c72ce0f4160ebdd8c82d36d93206918cfb3e
alagaesia93 <ale.macagno@hotmail.it>
2019-03-27 11:21:56 +0100

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['http://localhost:3000', 'https://topclean-backend.herokuapp.com']
 
     resource  '*',
               headers: :any,
